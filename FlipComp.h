0a2321c422add92632f76367c3038eeaa45a839e
AaronShea <aaron@aaronshea.me>
2018-07-10 23:06:30 -0700

    update for 15.0.4

--- FlipComp.h
+++ FlipComp.h
@@ -4,7 +4,7 @@
 // This file is to facilitate the export of dll methods when compiling this dll
 // and the import of other dll which would use the methods in this dll.
 
-#include "misc/MISC_Dylib.h"
+#include <BaseCore/misc/MISC_Dylib.h>
 
 #ifdef COMPILING_FLIPCOMP
 #define FLIPCOMP_PUBLIC       MISC_DYLIB_EXPORT
